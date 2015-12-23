c6c74351cbe71b84beb11a799835bb340edd071f
blueved verisk <vincent.ferre@blueved.com>
2015-12-23 13:05:16 -0600

    add comments

--- week2/cachematrix.R
+++ week2/cachematrix.R
@@ -1,45 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-## The assignment is regarding lexical scoping and caching functions that may require a long computation time. 
-## Specifically I am using solve() to find the inverse of a matrix and cache it using a free floating variable. 
-
-## Stored a function in a variable a<-makeCacheMatrix()
-## Run a$set(matrix(1:4,2,2)to store a matrix
-## Run cacheSolve(a)
-## Write a short comment describing this function
-
+## Build a customized matrix which provides cache
+## [in] : invertible matrix
+## [out]: a customized matrix which returns a list of 4 functions:
+## - set
+## - get
+## - setmatrix
+## - getmatrix
 
 makeCacheMatrix <- function(x = matrix()) {
-	  m<-NULL
-	  set<-function(y){
-	  x<<-y
-	  m<<-NULL
+	m<-NULL
+	## set function
+	set<-function(y){
+		x<<-y       ## access the parent environment variable 'x'
+		m<<-NULL	## access the parent environment variable 'm'
 	}
+	## source matrix accessor
 	get<-function() x
+	## calculates the inverse of the given matrix
 	setmatrix<-function(solve) m<<- solve
+	## inverse matrix accessor
 	getmatrix<-function() m
+	## returns a list
 	list(set=set, get=get,
-	   setmatrix=setmatrix,
-	   getmatrix=getmatrix)
+		setmatrix=setmatrix,
+		getmatrix=getmatrix)
 }
 
-## Write a short comment describing this function
+## Calculate the inverse of a given matrix 
+## [in]: matrix to invert - we assume it is invertible, there is no checking here
+## [out]: inverted matrix
 cacheSolve <- function(x=matrix(), ...) {
-	## Return a matrix that is the inverse of 'x'
+	## check if there is cached value for this matrix
     m<-x$getmatrix()
     if(!is.null(m)){
       message("*** getting cached data")
       return(m)
     }
-    matrix <- x$get() 
-    m<-solve(matrix, ...)
-    x$setmatrix(m)
-    m
+	## no cached value:
+    matrix <- x$get() 		## get the matrix to invert
+    m<-solve(matrix, ...)	## invert it
+    x$setmatrix(m)			## cache the value 
+    return(m)				## return the inverted matrix
 }
 
+
+## Test of the cache matrix 
 unitTest <- function(){
-	mt <- matrix(c(1,2,3,4), 2, 2)
-	cacheSolve(mt)
-	cacheSolve(mt)
-	
+	s <- matrix(1:4,2,2)	## test matrix, make sure it is invertible (det non null)
+	mt <- makeCacheMatrix(s)## build our customized super matrix
+	print('- first pass goes through calculation')
+	print(cacheSolve(mt))   ## calculate the inverse
+	print("- second pass should retrieve the cached value")
+	i<-cacheSolve(mt)		## re-calculate the inverse (using the cached value this time)
+	print("- product of both matrices should return identity matrix")
+	i %*% s					## matrix product of source and inverted matrices
 }
